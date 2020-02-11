260af40c6c9b903361f150ac20d45012ec3793c5
jeaze <joncoleman2012@gmail.com>
2020-02-11 15:10:26 +0000

    Update oslogtracker.sh

--- oslogtracker.sh
+++ oslogtracker.sh
@@ -9,17 +9,17 @@ fi
 
 echo Linux version detected: $(cat /etc/system-release) 
 
-echo Message logs since last boot >> $output
-#egrep -i "warning|critical|error|failed"  /var/log/messages | cut -c 17- | sort | uniq -c | sort -rn >> $output
-echo Priority 1 Emergency >> $output
+echo Message logs since last boot
+#egrep -i "warning|critical|error|failed"  /var/log/messages | cut -c 17- | sort | uniq -c | sort -rn
+echo Priority 1 Emergency
 journalctl -b -p 1..1 | cut -c 16-| sort | uniq -c | sort -rn | head -10
-echo Priority 2 Alert >> $output
+echo Priority 2 Alert
 journalctl -b -p 2..2 | cut -c 16-| sort | uniq -c | sort -rn | head -10
-echo Priority 3 Critical >> $output
+echo Priority 3 Critical
 journalctl -b -p 3..3 | cut -c 16-| sort | uniq -c | sort -rn | head -10
-echo Priority 4 Error >> $output
+echo Priority 4 Error
 journalctl -b -p 4..4 | cut -c 16-| sort | uniq -c | sort -rn | head -10
-echo Priority 5 Warning >> $output
+echo Priority 5 Warning
 journalctl -b -p 5..5 | cut -c 16-| sort | uniq -c | sort -rn | head -10
 echo Search for errors
 journalctl -b | egrep -i "warning|critical|error|failed" | cut -c 16- | sort | uniq -c | sort -rn | head -10
